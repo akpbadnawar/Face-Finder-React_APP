43f4805c27daeb5e6c676a3c4bc97648ee7ed64a
akpbadnawar <70686648+akpbadnawar@users.noreply.github.com>
2023-06-02 08:48:59 +0530

    First Push

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Log.css'
+
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0 container'>
+            <Tilt className='Tilt br2 shadow-2'>
+                <div className='Tilt-inner pa2' style={{ height: '150px', width:'150px'}}>
+                <img style={{paddingTop: '14px'}} src={brain} alt='logo'/>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo
