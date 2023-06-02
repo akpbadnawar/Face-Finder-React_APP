741650d4401aedd022046b7447bb3a729598d055
akpbadnawar <70686648+akpbadnawar@users.noreply.github.com>
2023-06-02 11:38:10 +0530

    updating the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import ParticlesBg from 'particles-bg'
+import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-       <>
-        <ParticlesBg type="cobweb" bg={true} />
-      </>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />    {/* 
-  <FaceRecognition/>*/}
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+
+  render(){  
+    return (
+      <div className="App">
+        <>
+          <ParticlesBg type="cobweb" bg={true} />
+        </>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>    
+        {/*<FaceRecognition/>*/}
+      </div>
+    );
+  }
 }
 
 export default App;
